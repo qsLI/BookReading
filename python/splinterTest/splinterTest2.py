ca1fdd1b11ec204b36e095217981109066d5809b
KL <liqisheng@whu.edu.cn>
2015-03-03 08:21:18 +0800

    splinterTest2 use baidu to search and download free source from csdn.splinterTest2-v2will comment once it finds a uncomment url

--- python/splinterTest/splinterTest2.py
+++ python/splinterTest/splinterTest2.py
@@ -1,56 +1,102 @@
 #coding=utf-8
 import os,time
-import random
 from splinter import Browser
+from threading import *
+# class closeWindow(Thread):
+#     def __init__(self,browser):
+#         super(closeWindow, self).__init__()
+#         self.browser = browser
+#     def run(self):
+#         time.sleep(50)
+#         print "time out kill window 0"
+#         self.browser.windows[1].close()
+#         self.stop()
+#     def stop(self):
+#         thread.exit_thread()
 
-#随机评论
-commentsList = [u"不错的资源！！！！！！！",u"谢谢楼主分享！",u"还不错，谢谢",u"还没看,先下载",u"感谢分享，nice",u"很好的东西，非常不错",u"用这个很方便!!",u"这个很不错！"]
-print random.choice(commentsList)
-browser = Browser('chrome')
-# Visit URL
-url = "http://passport.csdn.net/account/login"
-browser.visit(url)
-# browser.fill('f', 'splinter - python acceptance testing for web applications')
-# Find and click the 'search' button
-btnEmail = browser.find_by_id('username')
-btnPasswd = browser.find_by_id('password')
+def login(browser,username="",passwd=""):
+    """登录csdn"""
 
-btnEmail.fill('')#用户名
-btnPasswd.fill('')#密码
-#print dir(browser)
-# Interact with elements
-btnSubmit = browser.find_by_value("登 录")
-btnSubmit.click()#登录
-# button.click()
-#time.sleep(10)
-time.sleep(6)
-browser.visit("http://download.csdn.net/my/downloads")
-
-while browser.is_element_not_present_by_css(".pageliststy"):
-    time.sleep(0.1)
-urls = [ url['href'] for url in  browser.find_link_by_text("立即评价，通过可返分")]
-#总页面个数
-pages = int(browser.find_by_css(".pageliststy")[-1]['href'].split('/')[-1])
-for index in range(2,pages+1):
-    browser.visit("http://download.csdn.net/my/downloads/%d"%index)
-    urls = urls + [ url['href'] for url in  browser.find_link_by_text("立即评价，通过可返分")]
-    time.sleep(1)
-print urls
-for url in urls:
-    print url
+    url = "http://passport.csdn.net/account/login"
     browser.visit(url)
-    while browser.is_element_not_present_by_value("5"):
-        time.sleep(0.1)
-    startControl = browser.find_by_css(".star-rating")[-1]# -1 for 5 stars  -2 for 4 stars  1 for one star  also may  random
-    textControl = browser.find_by_id("cc_body")[0]#评论框
-    btnComment = browser.find_by_value("发表评论")[0]#提交框
-    #print dir(startControl)
-    print "loaded"
-    startControl.mouse_over()#鼠标到评分栏
-    startControl.click()    #鼠标点击
-    textControl.fill(random.choice(commentsList))#填写评论
-    btnComment.click()#评论提交
-    alert = browser.get_alert()#处理 js的alert确认
-    alert.accept()#确定
-    time.sleep(60)#评论间隔要有一分钟
-print "Hello Kitty"
+    # browser.fill('f', 'splinter - python acceptance testing for web applications')
+    # Find and click the 'search' button
+    btnEmail = browser.find_by_id('username')
+    btnPasswd = browser.find_by_id('password')
+    btnRemember = browser.find_by_name('rememberMe')
+
+
+    btnEmail.fill(username)#用户名
+    btnPasswd.fill(passwd)#密码
+    btnRemember.check()#
+    #print dir(browser)
+    # Interact with elements
+    btnSubmit = browser.find_by_value("登 录")
+    btnSubmit.click()#登录
+    time.sleep(8)
+    # button.click()
+
+
+def searchFree(browser):
+    """用百度搜索免费的资源，返回资源的列表"""
+
+    url = "http://www.baidu.com"
+    browser.visit(url)
+    btnKeyWord = browser.find_by_id('kw')[0]
+    btnKeyWord.fill(u'"资源积分:0分"  联通下载 qt  site:download.csdn.net')#密码
+    btnSubmit = browser.find_by_id("su")[0]
+    btnSubmit.click()#提交搜素
+
+    base = 0
+    time.sleep(4)    
+    for page in range(1,90):        
+        # timerThread = closeWindow(browser)
+        # timerThread.start()
+        print base," base"
+        print page," page"
+        for x in range(base+1,base+11):
+            print x
+            time.sleep(6)
+            urlTmp = browser.find_by_xpath('//*[@id="%d"]/h3/a'%x)[0]
+            # time.sleep(10)
+            urlTmp.click()
+            browser.windows.current = browser.windows[1]
+   
+            try:
+                print "try to clcik"
+                # browser.execute_script('window.scrollTo(0, 400);')
+                browser.execute_script('window.scrollTo(0, 200);')
+                time.sleep(15)                     
+                browser.execute_script('window.scrollTo(0, 400);')
+                btnDownload = browser.find_by_css(r'#wrap > div.bd.clearfix > div.article > div.information.mb-bg.clearfix > div.info > a.btn.btn-primary.btn-lg.WCDMA.btndownload')[0]
+                btnDownload.click()    
+                time.sleep(10)      
+                btnDownload = browser.find_by_css(r'#download_btn2')[0]
+                btnDownload.click()    
+                # btnDownload = browser.find_by_xpath(r'//*[@id="download_btn2"]')[0]
+                # btnDownload.click()  
+                time.sleep(10)
+                btnConfirm = browser.find_by_xpath(r'//*[@id="user_score_btn"]')[0]
+                btnConfirm.click()
+                print "done"
+            except:
+                try:
+                    btnConfirm = browser.find_by_xpath(r'//*[@id="user_score_btn"]')[0]
+                    btnConfirm.click()
+                    time.sleep(5)
+                    print "done"
+                except:
+                    print "error"        
+            browser.windows.current = browser.windows[0]
+            browser.windows.current.close_others() #关闭窗口
+            # timerThread.stop()
+        base = base + 10    
+        nextPage = browser.find_link_by_text(u"下一页>")[0] 
+        nextPage.click()
+
+
+if __name__ == "__main__":
+    browser = Browser('chrome')
+    login(browser)
+    searchFree(browser)
+    print "Hello Kitty"
