4312f9f042b9348c4cc5ceb82468e2acef1a935e
liqisheng@whu.edu.cn <liqisheng@whu.edu.cn>
2015-03-05 13:34:48 +0800

    python RPC test

---
+++ python/RPC/rpc-client.py
@@ -0,0 +1,12 @@
+﻿# -*- coding: utf-8 -*-
+from xmlrpclib import ServerProxy
+import sys
+if __name__ == '__main__':
+	s = ServerProxy("http://192.168.1.189:8080",verbose=0,use_datetime=1,allow_none=True)
+	#print s.add(3,4)
+	reload(sys)   
+	sys.setdefaultencoding('utf8')
+	print sys.getdefaultencoding()
+	print s.writeMessage(u'你好，世界')
+	print s.writeMessage(u'我想和你谈谈')
+	print s.writeMessage(u'让我们创造更好的世界')
---
+++ python/RPC/rpc-server.py
@@ -0,0 +1,21 @@
+﻿#coding:utf-8
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+import sys
+def add(x,y):
+	return x + y
+def hello(name):
+	print "hello, " + name
+def writeMessage(name):
+	f = open("123.txt",'a')
+	f.write(name+"\n")
+	f.close()
+	return "Your message is writen to 123.txt"
+if __name__ == "__main__":
+	reload(sys)   
+	sys.setdefaultencoding('utf8')
+	print sys.getdefaultencoding()
+	s = SimpleXMLRPCServer(('192.168.1.189',8080),encoding="utf8",allow_none=True)
+	s.register_function(add)
+	s.register_function(hello)
+	s.register_function(writeMessage)
+	s.serve_forever()
